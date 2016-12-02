d5c129a62f9275cd87a7346da8939f0eb27bb1a6
dataricc <rcsmith.dev@gmail.com>
2016-12-02 09:15:31 -0500

    Pushing my changes in order to get the version Connor and I worked on the login and ajax

--- Splice/src/main/webapp/js/app.js
+++ Splice/src/main/webapp/js/app.js
@@ -1,29 +1,18 @@
 var app = angular.module("spliceGame", []);
 
-app.controller("loginCtrl", function loginCtrl($scope){
-	
-	//$scope.
-	
-});
-
-app.controller("logoutCtrl", function logoutCtrl($scope){
-	
-});
-
-app.directive("loginBox", function() {
-	return{
-		templateURL:"login-box.html"
-	};
-});
-
-app.directive("logoutBox", function() {
-	return{
-		templateURL:"logout-box.html"
-	};
-});
-
-//html
-// <login-box></login-box>
-
-//login-box.html
-// <div> form </div>
+app.controller('GetUserCtrl', ['$scope', '$http', function ($scope, $http) 
+{
+	function getUserData() {
+        $http({
+            method: 'POST',
+            url: 'GameServlet',
+            headers: {'Content-Type': 'application/json'},
+            data: data
+        }).success(function (data, status, headers, config) {
+            $scope.user = data;
+        }).error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+}]);
