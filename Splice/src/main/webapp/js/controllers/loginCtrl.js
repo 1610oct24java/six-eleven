a74900c6eb3ffa7ee233fe20fc782e8853c50936
Ryan Lawson <ryan.lawson2@gmail.com>
2016-12-05 10:40:08 -0500

    Update loginCtrl.js

--- Splice/src/main/webapp/js/controllers/loginCtrl.js
+++ Splice/src/main/webapp/js/controllers/loginCtrl.js
@@ -8,7 +8,7 @@ app.controller("loginController", function($scope, $http) {
 	// this function pulls the login info from the textfields and sends as a http post
 	$scope.getUsername = function(){
 		// JSON object
-		var loginData = JSON.stringify({userId: -1, username: $scope.user, password: $scope.pass});
+		var loginData = JSON.stringify({username: $scope.user, password: $scope.pass});
 		
 		postData(loginData);
 		console.log("Username: " + $scope.user + " Password: " + $scope.pass);
