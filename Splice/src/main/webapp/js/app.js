c51c70f8caa99019b52128c89da1685780d5a7ad
dataricc <rcsmith.dev@gmail.com>
2016-12-06 22:07:55 -0500

    login fixed. working on lobby controller

--- Splice/src/main/webapp/js/app.js
+++ Splice/src/main/webapp/js/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var app = angular.module("gameApp", ["ngRoute"]); 
 
 app.config(function($routeProvider){
@@ -17,9 +15,9 @@ app.config(function($routeProvider){
 		templateUrl : "pages/queue-box.html",
 		controller : "queueController"
 	})
-	.when("/error404", {
-		templateUrl : "pages/error404.html",
-		controller : "errorController"
+	.when("/game", {
+		templateUrl : "game.html",
+		controller : "gameController"
 	})
 	.otherwise({
 		redirectTo : "/login"
@@ -30,14 +28,16 @@ app.controller("loginController", function($scope, $http, $location) {
 
 	$scope.user;
 	$scope.pass;
+	$scope.registerUser;
+	$scope.registerPass;
 	
 	// this function pulls the login info from the textfields and sends as a http post
 	$scope.getUsername = function(){
 		// JSON object
-		var loginData = JSON.stringify({username: $scope.user, password: $scope.pass});
+		var loginData = JSON.stringify({username: this.user, password: this.pass});
 		
-		postData(loginData);
-		console.log("Username: " + $scope.user + " Password: " + $scope.pass);
+		postLoginData(loginData);
+		console.log("Username: " + this.user + " Password: " + this.pass);
 		console.log(loginData); 
 	}
 	
@@ -46,7 +46,7 @@ app.controller("loginController", function($scope, $http, $location) {
 	}
 	
 	// postData takes in JSON, sends with HTTP POST to given servlet url
-	function postData(data){
+	function postLoginData(data){
 		$http({
 			method: 'POST',
 			url: 'Login.do',
@@ -61,6 +61,33 @@ app.controller("loginController", function($scope, $http, $location) {
 
 app.controller("lobbyController", function($scope, $http) {
 	
+	
+	$scope.joinGame = function(){
+		var loginData = JSON.stringify({});
+		
+		postGameListData(loginData);
+		console.log(loginData);
+	}
+	
+	function postGameListData(data){
+		$http({
+			method: 'POST',
+			url: 'Login.do',
+			headers: {'Content-Type': 'application/json'},
+			data: data
+		}).success(function (output){
+			//console.log(JSON.parse(output));
+			$location.path("/lobby");
+		});
+	}
+	
+	$http.get("some url")
+	.then(function (response) {$scope.games = response.data.games;});
+
+	$scope.chatlog = [];
+	$scope.sendMessage = function(){
+		$scope.chatlog.push($scope.message);
+	}
 })
 
 app.controller("queueController", function($scope, $http) {
