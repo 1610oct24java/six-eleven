41285105418bf6c7708b238ad5f09b1b48ff5182
Christian Gonzalez <Christian Gonzalez>
2016-12-11 19:11:52 -0500

    Style tweaks, displays lab now, angular work

--- Splice/src/main/webapp/res/js/app.js
+++ Splice/src/main/webapp/res/js/app.js
@@ -1,122 +1,137 @@
-var app = angular.module("gameApp", ["ngRoute"]); 
+var app = angular.module("gameApp", ["ngRoute"]);
 
 var authUser = null;
 var sorcerers = [
-     	        {
-     	            name: 'Xanitov, Radiant Husk',
-     	            vit: 5,
-     	            pow: 6,
-     	            def: 5,
-     	            spd: 6,
-     	            int: 7,
-     	            flavor: "Power.  It comes at a cost.  Xanitov knew that better than most people knew how to breathe.  He understood the risks, he understood the pain.  But he also understood that it was worth it.",
-     	            favStat: 2,
-     	            dmpStat: 5,
-     	            image: "stickSorc.png"
-     	        }
-     	    ];
-     	    
-     	    var creatures = [
-     	        {
-     	            name: 'Grand Hart',
-     	            vit: 5,
-     	            pow: 2,
-     	            def: 2,
-     	            spd: 2,
-     	            int: 0,
-     	            flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
-     	            favStat: 2,
-     	            dmpStat: 5,
-     	            image: "grand_hart.png"
-     	        },
-     	        {
-     	            name: 'Crag Wasp',
-     	            vit: 4,
-     	            pow: 3,
-     	            def: 2,
-     	            spd: 2,
-     	            int: 1,
-     	            flavor: "Their hives keep the drakes from moving to the warmer southern peaks.",
-     	            favStat: 4,
-     	            dmpStat: 1,
-     	            image: "crag_wasp.png"
-     	        },
-     	        {
-     	            name: 'Wyld Rat',
-     	            vit: 5,
-     	            pow: 2,
-     	            def: 2,
-     	            spd: 2,
-     	            int: 0,
-     	            flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
-     	            favStat: 2,
-     	            dmpStat: 5,
-     	            image: "wyld_rat.png"
-     	        },
-     	        {
-     	            name: 'Great Mantis',
-     	            vit: 5,
-     	            pow: 2,
-     	            def: 2,
-     	            spd: 2,
-     	            int: 0,
-     	            flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
-     	            favStat: 2,
-     	            dmpStat: 5,
-     	            image: "great_mantis.png"
-     	        },
-     	        {
-     	            name: 'Cliffside Stalker',
-     	            vit: 5,
-     	            pow: 2,
-     	            def: 2,
-     	            spd: 2,
-     	            int: 0,
-     	            flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
-     	            favStat: 2,
-     	            dmpStat: 5,
-     	            image: "cliffside_stalker.png"
-     	        }
-     	    ];
-     	    
-     	    var game = {
-     	        players: [],
-     	        wylds: [],
-     	        deck: [],
-     	        state: {
-     	            round: 1,
-     	            turn: 0,
-     	            phase: 1,
-     	        }
-     	    }
-     	    
-     	    game.deck = creatures;
+        {
+            name: 'Xanitov, Radiant Husk',
+            vit: 5,
+            pow: 6,
+            def: 5,
+            spd: 6,
+            int: 7,
+            flavor: "Power.  It comes at a cost.  Xanitov knew that better than most people knew how to breathe.  He understood the risks, he understood the pain.  But he also understood that it was worth it.",
+            favStat: 2,
+            dmpStat: 5,
+            image: "stickSorc.png"
+        },
+        {
+            name: 'Other, the Different',
+            vit: 5,
+            pow: 6,
+            def: 5,
+            spd: 6,
+            int: 7,
+            flavor: "Power.  It comes at a cost.  Xanitov knew that better than most people knew how to breathe.  He understood the risks, he understood the pain.  But he also understood that it was worth it.",
+            favStat: 2,
+            dmpStat: 5,
+            image: "stickSorc.png"
+        }
+    ];
 
-app.config(function($routeProvider){
+
+var creatures = [
+    {
+        name: 'Grand Hart',
+        vit: 5,
+        pow: 2,
+        def: 2,
+        spd: 2,
+        int: 0,
+        flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
+        favStat: 2,
+        dmpStat: 5,
+        image: "grand_hart.png"
+    },
+    {
+        name: 'Crag Wasp',
+        vit: 4,
+        pow: 3,
+        def: 2,
+        spd: 2,
+        int: 1,
+        flavor: "Their hives keep the drakes from moving to the warmer southern peaks.",
+        favStat: 4,
+        dmpStat: 1,
+        image: "crag_wasp.png"
+    },
+    {
+        name: 'Wyld Rat',
+        vit: 5,
+        pow: 2,
+        def: 2,
+        spd: 2,
+        int: 0,
+        flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
+        favStat: 2,
+        dmpStat: 5,
+        image: "wyld_rat.png"
+    },
+    {
+        name: 'Great Mantis',
+        vit: 5,
+        pow: 2,
+        def: 2,
+        spd: 2,
+        int: 0,
+        flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
+        favStat: 2,
+        dmpStat: 5,
+        image: "great_mantis.png"
+    },
+    {
+        name: 'Cliffside Stalker',
+        vit: 5,
+        pow: 2,
+        def: 2,
+        spd: 2,
+        int: 0,
+        flavor: "A noble creature with a savage secret: A hart's horns will only grow a new point the next season if they spill hot blood.",
+        favStat: 2,
+        dmpStat: 5,
+        image: "cliffside_stalker.png"
+    }
+];
+
+var players = [
+    {
+        username: "aushou",
+        mySorc: sorcerers[0],
+        researchPool: 0,
+        lab: [creatures[3]]
+    },
+    {
+        username: "fishspit",
+        mySorc: sorcerers[1],
+        researchPool: 0,
+        lab: [creatures[2]]
+    }
+];
+
+app.config(function ($routeProvider) {
 	
 	$routeProvider
-	.when("/login", {
-		templateUrl : "login-box.html",
-		controller : "loginController"
-	})
-	.when("/lobby", {
-		templateUrl : "lobby-box.html",
-		controller : "lobbyController"
-	})
-	.when("/queue", {
-		templateUrl : "queue-box.html",
-		controller : "queueController"
-	})
-	.when("/game", {
-		templateUrl : "game.html",
-		controller : "gameController"
-	})
-	.otherwise({
-		redirectTo : "/login"
-	})
+        .when("/login", {
+            templateUrl : "login-box.html",
+            controller : "loginController"
+        })
+        .when("/lobby", {
+            templateUrl : "lobby-box.html",
+            controller : "lobbyController"
+        })
+        .when("/queue", {
+            templateUrl : "queue-box.html",
+            controller : "queueController"
+        })
+        .when("/game", {
+            templateUrl : "game.html",
+            controller : "gameController"
+        })
+        .otherwise({
+            redirectTo : "/login"
+        });
 });
 
-app.controller("loginController", function($scope, $http, $location) {
+app.controller("loginController", function ($scope, $http, $location) {
 
 	$scope.user;
 	$scope.pass;
@@ -124,9 +139,9 @@ app.controller("loginController", function($scope, $http, $location) {
 	$scope.registerPass;
 
 	// this function pulls the login info from the textfields and sends as a http post
-	$scope.doLogin = function(){
+	$scope.doLogin = function() {
 		// JSON object
-		var loginData = {username:this.user,password:this.pass};
+		var loginData = {username: this.user, password: this.pass};
 		
 		postLoginData(loginData, this.user);
 		
@@ -338,17 +353,46 @@ app.controller("queueController", function($scope, $http, $location) {
 	
 });
 
-app.controller('WyldsController', function () {
-    this.creatures = [creatures[0], creatures[1], creatures[2], creatures[3]];
-});
-
-app.controller('PlayerController', function () {
-    this.sorcerer = sorcerers[0];
-});
-
 app.controller("gameController", function($scope, $http, $location) {
-	
-	
+	this.wylds = [creatures[0], creatures[1], creatures[4], creatures[2]];
+    this.players = [players[0], players[1]];
+    this.state = {
+        round: 1,
+        turn: 0,
+        phase: 1
+    };
+    
+    this.getTurn = function () {
+        return players[this.state.turn].username;
+    }
+    
+    this.getPhase = function () {
+        if (this.state.phase == 0) {
+            return "Environment";
+        }
+        if (this.state.phase == 1) {
+            return "Research";
+        }
+        if (this.state.phase == 2) { 
+            return "Combat";
+        }
+    }
+    
+    $scope.attack = function (input) {
+        
+        var packet = {
+            command: "attack",
+            data: input,
+            from: this.getTurn()
+        }
+        
+        console.log("Attack: " + input);
+    }
+    $scope.research = function (input) {
+        
+        console.log("Research: " + input);
+    }
+    
 });
 
 
